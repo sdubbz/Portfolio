43a7dd75ddf965266df1aaf0c83c7723f77fc8f5
scott <scottw91@live.ca>
2022-02-10 21:34:10 -0500

    fixed tailwind setup

--- client/components/home.js
+++ client/components/home.js
@@ -8,9 +8,11 @@ export const Home = () => {
       <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <button class="bg-violet-500 hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 ...">
+  Save changes
+</button>
 
       <h1>cmonnn</h1>
-      <button></button>
     </div>
   )
 
