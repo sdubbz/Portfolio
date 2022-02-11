c7577254ca752190ae98c09c49c7e4d1edcec803
scott <scottw91@live.ca>
2022-02-10 23:11:24 -0500

    some initial nav bar stuff

--- client/components/home.js
+++ client/components/home.js
@@ -5,10 +5,23 @@ export const Home = () => {
 
   return(
     <div>
+
+      <nav class="container flex justify-around py-8 mx-auto bg-white">
+  <div>
+    <h3 class="text-1xl font-medium text-blue-500">Dubbs designs</h3>
+  </div>
+  <div class="space-x-8">
+    <a href="">Home</a>
+    <a href="">About Us</a>
+    <a href="">Blogs</a>
+    <a href="">Our Team</a>
+    <a href="">Contact Us</a>
+  </div>
+</nav>
       <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
-  <button class="bg-violet-500 hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 ...">
+ <button class="bg-violet-500 hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 ...">
   Save changes
 </button>
 
