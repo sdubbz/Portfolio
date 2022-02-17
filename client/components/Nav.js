0559ca97af14e30201cd00fe09c6431cdfa86eb2
scott <scottw91@live.ca>
2022-02-16 21:40:06 -0500

    changed nave bar colors

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,12 +1,26 @@
-import React from 'react';
-
+import React from "react";
 
 export const Nav = () => {
-
-
   return (
-    <div>FUCK YOU</div>
-  )
+    <div class="text-blue-500">
+      <nav class="container flex justify-around py-8 mx-auto bg-white items-center px-8 md:px-14 lg:px-24 w-full text-black">
+        <div>
+          <h3 class="text-4xl font-bold text-blue-500">Dubbs designs</h3>
+        </div>
+        <div class=" hidden md:flex space-x-12 items-center text-blue-500 a:link">
+          <a class="text-blue-500" href="">Home</a>
+          <a class="text-blue-500" href="">About Us</a>
+          <a class="text-blue-500" href="">Blogs</a>
+          <a class="text-blue-500" href="">Our Team</a>
+          <a class="text-blue-500" href="">Contact Us</a>
+          <button class="px-6 py-2 bg-theme text-white font-bold">Hire me</button>
+        </div>
+        <div class="md:hidden">
+          <img className="mobileMenu" src="./images/mobileMenu.png"></img>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
--- client/components/home.js
+++ client/components/home.js
@@ -3,18 +3,7 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
-      <nav class="container flex justify-around py-8 mx-auto bg-white items-center px-8 md:px-14 lg:px-24 w-full">
-        <div>
-          <h3 class="text-4xl font-medium text-blue-500">Dubbs designs</h3>
-        </div>
-        <div class="space-x-8">
-          <a href="">Home</a>
-          <a href="">About Us</a>
-          <a href="">Blogs</a>
-          <a href="">Our Team</a>
-          <a href="">Contact Us</a>
-        </div>
-      </nav>
+
     </div>
   );
 };
