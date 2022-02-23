3645ba5ce00b07aaf59f5da02f7bb007376fefaf
scott <scottw91@live.ca>
2022-02-22 21:11:24 -0500

    bouncing logo in

--- client/components/canvas.js
+++ client/components/canvas.js
@@ -1,11 +1,89 @@
-import React, {useRef} from "react";
-
+import React, {useEffect, useRef} from "react";
+// import mobileMenu from '../../public/images/mobileMenu.png'
 
 
 const Canvas = props => {
 
+let speed = 20;
+let scale = 0.17; // Image scale (I work on 1080p monitor)
+
+let ctx;
+let logoColor;
+
+let dvd = {
+    x: 200,
+    y: 300,
+    xspeed: 5,
+    yspeed: 5,
+    img: new Image(),
+
+};
+
 
 const canvasRef = useRef(null)
+
+useEffect(() => {
+
+
+
+
+   let canvas = canvasRef.current;
+    ctx = canvas.getContext("2d");
+    dvd.img.src = './images/mobileMenu.png';
+
+    //Draw the "tv screen"
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+
+
+function update() {
+    setTimeout(() => {
+        //Draw the canvas background
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw DVD Logo and his background
+        ctx.fillStyle = logoColor;
+        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        //Move the logo
+        dvd.x+=dvd.xspeed;
+        dvd.y+=dvd.yspeed;
+        //Check for collision
+        checkHitBox();
+        update();
+    }, speed)
+}
+
+//Check for border collision
+function checkHitBox(){
+    if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
+        dvd.xspeed *= -1;
+        pickColor();
+    }
+
+    if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
+        dvd.yspeed *= -1;
+        pickColor();
+    }
+}
+
+//Pick a random color in RGB format
+function pickColor(){
+    let r = Math.random() * (254 - 0) + 0;
+    let g = Math.random() * (254 - 0) + 0;
+    let b = Math.random() * (254 - 0) + 0;
+
+    logoColor = 'rgb('+r+','+g+', '+b+')';
+}
+
+pickColor();
+update();
+
+},[])
+
+
 
  return <canvas ref={canvasRef} {...props}/>
 }
