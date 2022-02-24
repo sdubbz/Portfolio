a879f083d061f8f4f2d4f417e8fd0f3823866ab9
scott <scottw91@live.ca>
2022-02-23 20:23:42 -0500

    weve done it lads, bouncing text

--- client/components/canvas.js
+++ client/components/canvas.js
@@ -5,6 +5,7 @@ import React, {useEffect, useRef} from "react";
 const Canvas = props => {
 
 let speed = 20;
+let letterSpeed = 20;
 let scale = 0.50; // Image scale (I work on 1080p monitor)
 
 let ctx;
@@ -20,13 +21,20 @@ let dvd = {
 };
 
 let hire = {
+  text:'Hire me!',
   x: 200,
   y: 300,
   xspeed: 5,
   yspeed: 5,
-
+  height: 30
 
 };
+// var x = 300;
+// var y = 200;
+// var dx = 3;
+// var dy = 3;
+// var height = 30;
+// var text = "hire me";
 
 
 const canvasRef = useRef(null)
@@ -50,23 +58,66 @@ useEffect(() => {
 function update() {
     setTimeout(() => {
         //Draw the canvas background
+
         ctx.fillStyle = '#000';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         //Draw DVD Logo and his background
-        ctx.fillStyle = logoColor;
-        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
-        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        // ctx.fillStyle = logoColor;
+        // ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        // ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
         //Move the logo
-        dvd.x+=dvd.xspeed;
-        dvd.y+=dvd.yspeed;
-        ctx.font      = "normal 36px Verdana";
-ctx.fillStyle = "#ffffff";
-ctx.fillText(hire, 200, 200);
+        // dvd.x+=dvd.xspeed;
+        // dvd.y+=dvd.yspeed;
+//         ctx.font      = "normal 36px Verdana";
+// ctx.fillStyle = "#ffffff";
+
+// ctx.fillText(hire.text, letterSpeed, letterSpeed);
+
         //Check for collision
+        function animate () {
+          var metrics = ctx.measureText(hire.text);
+          var width = metrics.width;
+          // requestAnimationFrame(animate);
+          // ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+          ctx.font = "bold 30px Helvetica";
+          ctx.fillStyle = logoColor;
+          ctx.fillText(hire.text, hire.x, hire.y);
+
+
+          if ((hire.x + width > innerWidth)||(hire.x <= 0)) {
+              hire.xspeed = -hire.xspeed;
+              pickColor();
+          }
+
+          if ((hire.y > innerHeight)||(hire.y <= hire.height/2)) {
+              hire.yspeed = -hire.yspeed;
+              pickColor();
+          }
+          hire.x += hire.xspeed;
+          hire.y += hire.yspeed;
+      }
+      animate();
+        // checkHitBoxx()
         checkHitBox();
         update();
     }, speed)
+
+
 }
+
+// function checkHitBoxx(){
+//   if(hire.xspeed >= canvas.width || hire.x <= 0){
+//       letterSpeed *= -1;
+//       pickColor();
+//   }
+
+//   if(hire.yspeed >= canvas.height || hire.y <= 0){
+//       letterSpeed *= -1;
+//       pickColor();
+//   }
+// }
+
 
 //Check for border collision
 function checkHitBox(){
