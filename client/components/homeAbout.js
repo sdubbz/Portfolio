241086fc8b92a68701b35a4a99d042b7f3752f6b
scott <scottw91@live.ca>
2022-07-26 20:46:11 -0400

    moving div center

--- client/components/homeAbout.js
+++ client/components/homeAbout.js
@@ -8,7 +8,7 @@ export const HomeAbout = () => {
     // <h3 className='homeAbout'>Hello my name is Scott Wilson, welcome to my  portfolio website. If you havn't guessed yet, I was a carreer bartender before transition so software engineering. Take a look around at my projects section, or learn more about me in the Scott'sTales section, maybe grab a cocktail while your at it. </h3>
     <div class="flex border-solid hover:scale-150 border-4 homeAbout mx-auto items-center justify-evenly grow-[3] hover:place-content-center ">
     <img class="scott hover:place-content-center" src="/images/scott.jpeg"></img>
-    <p class="text-xl  hover:place-content-center" >
+    <p class="text-xl  hover:text-align: center" >
     Hello my name is Scott Wilson, welcome to my  portfolio website. If you havn't guessed yet, I was a carreer bartender before transition so software engineering. Take a look around at my projects section, or learn more about me in the Scott'sTales section, maybe grab a cocktail while your at it??
    </p>
    </div>
